70219576a87f2b375690f6576457524a9e2fb31a
Peng Qingyong <qingyong.peng@extantfuture.com>
2018-07-27 14:28:01 +0800

    nothing

--- get_good_st.py
+++ get_good_st.py
@@ -6,8 +6,16 @@ import jingzhuan
 import time
 
 
+def get_plate_trend():
+    
+
+
+
+
+
 
 def get_st():
     st_buy_list=list()
     stlist=ts.get_stock_basics()
+    plates=get_plate_trend()
     for i in stlist:
---
+++ test2.py
@@ -0,0 +1,5 @@
+import tushare as ts
+import time
+a=ts.get_industry_classified()
+
+time.sleep(1)
