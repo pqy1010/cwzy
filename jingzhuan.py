ee7075e37aa163c480abf2ecffb4825c7dc42d90
Peng Qingyong <qingyong.peng@extantfuture.com>
2018-08-07 15:18:01 +0800

    it is work

--- jingzhuan.py
+++ jingzhuan.py
@@ -64,10 +64,10 @@ def zhulikongpan(data):
     N1=3
     a=time.time()
     B1 = (HHV(H, N) - C) / (HHV(H, N) - LLV(L, N)) * 100 - M# 此处慢的原因是H,L等都是pd.Serves,如果只取其值则很快.
-    print('B1 use %f' % (time.time() - a))
+    # print('B1 use %f' % (time.time() - a))
     a=time.time()
     B2 = SMA(B1, N, 1) + 100
-    print('B2 use %f' % (time.time() - a))
+    # print('B2 use %f' % (time.time() - a))
     B3 = (C - LLV(L, N)) / (HHV(H, N) - LLV(L, N)) * 100
     B4 = SMA(B3, 3, 1)
     B5 = SMA(B4, 3, 1) + 100
--- main.py
+++ main.py
@@ -11,57 +11,65 @@ import time
 money=10000
 tradelist=pd.Series(index=['code','b_time','b_price','b_money','b_count','owntime','state','earn','s_price'])
 
-
-
-
-
-
-
-
+def stockjudge(info):
+    buypoint = int(time.mktime(time.strptime(info['buy'][0], '%Y-%m-%d')))
+    # nowtime = time.time()
+    nowtime=int(time.mktime(time.strptime(time.strftime('%Y-%m-%d'),'%Y-%m-%d')))
+    diffx1x2=info['x1']-info['x2']
+    if nowtime - buypoint <= 3 * 24 * 3600:
+        if info['kongpan'][0] > 0:
+            if diffx1x2[0]>=0:
+                return 1
+    return 0
 
 def select_golden_data():
     datalist=ts.get_stock_basics()
     datalist=datalist.reset_index()
     stocklist=datalist['code']
     stocknum=len(stocklist)
+
     buylist=[]
     for i in range(stocknum):
         dayK=ts.get_k_data(stocklist[i])
         dayK=dayK[::-1]
+        if dayK.shape[0]<=60:
+            continue
         buy, sell = jingzhuan.huiyanKxian(dayK)
         x1, x2 = jingzhuan.bulaojijie(dayK)
         kongpan = jingzhuan.zhulikongpan(dayK)
-
-        buypoint=int(time.mktime(time.strptime(buy[0], '%Y-%m-%d')))
-        nowtime=time.time()
-        if nowtime-buypoint<=2*24*3600:
-
-
-select_golden_data()
+        stockinfo={'data':dayK,'buy':buy,'sell':sell,'x1':x1,'x2':x2,'kongpan':kongpan}
+        judge_flag=stockjudge(stockinfo)
+        if judge_flag==1:
+            buylist.append(stocklist[i])
+    return buylist
 
 
 
+buylist=select_golden_data()
 
 
-a=time.time()
-data=ts.get_hist_data('000063') #一次性获取全部日k线数据
-print('load data use %f'%(time.time()-a))
+## test code------------------------------------------------------
 
-
-a=time.time()
-buy,sell=jingzhuan.huiyanKxian(data)
-print('huiyankxian use %f'%(time.time()-a))
-
-
-a=time.time()
-x1,x2=jingzhuan.bulaojijie(data)
-print('bulaojijie use %f'%(time.time()-a))
-
-a=time.time()
-kongpan=jingzhuan.zhulikongpan(data)
-print('zhulikongpan use %f'%(time.time()-a))
-plt.figure()
-plt.plot(kongpan)
-plt.show()
-a=time.time()
-time.sleep(1)
+#
+# a=time.time()
+# data=ts.get_hist_data('000063') #一次性获取全部日k线数据
+# print('load data use %f'%(time.time()-a))
+#
+#
+# a=time.time()
+# buy,sell=jingzhuan.huiyanKxian(data)
+# print('huiyankxian use %f'%(time.time()-a))
+#
+#
+# a=time.time()
+# x1,x2=jingzhuan.bulaojijie(data)
+# print('bulaojijie use %f'%(time.time()-a))
+#
+# a=time.time()
+# kongpan=jingzhuan.zhulikongpan(data)
+# print('zhulikongpan use %f'%(time.time()-a))
+# plt.figure()
+# plt.plot(kongpan)
+# plt.show()
+# a=time.time()
+# time.sleep(1)
