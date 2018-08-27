ad216570a1ffd0332bbe72f2a41016b5e4e9e857
qingyong <pqy1010@126.com>
2018-08-27 23:37:26 +0800

    debug

--- jingzhuan.py
+++ jingzhuan.py
@@ -1,8 +1,16 @@
 import tushare as ts
+import pandas as pd
 # from tdx import HHV as HHV
 # from tdx import LLV as LLV
+from tongdaxin import EMA as EMA
+from tongdaxin import SMA as SMA
+from tongdaxin import CROSS as CROSS
+from tongdaxin import MA  as MA
+from tongdaxin import REF as REF
+from tongdaxin import HHV as HHV
+from tongdaxin import LLV as LLV
 import time
-
+import numpy as np
 
 
 
--- pqymodle.py
+++ pqymodle.py
@@ -14,39 +14,45 @@ import time
 
 
 def cwzy_get_hist_data(id):
-
-    if int(id[0])==3:
+    thistime=int(time.time()*1000)
+    if id[0:3]=='300'or id[0:3]=='000'or id[0:3]=='002':# 深证
         rtntype = 5
         token = '4f1862fc3b5e77c150a2b985b12db0fd'
-        cb = 'jQuery18307611704805113713_1535357772151'
-        newid = 'id'+'2'
-        type = 'r'
-        iscr = 'false'
-        xiahuaxian = '1535357774245'
-        urls = r'http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=%d&token=%s&cb=%s&id=%s&type=%s&iscr=%s&_=%s' % (rtntype,token, cb, newid,type,iscr,xiahuaxian)
-    elif int(id[0])==0 or int(id[0])==6:
-        jQ = r'jQuery1830584617836716971_1535355855473'
-        token = r'4f1862fc3b5e77c150a2b985b12db0fd'
-        urls = r'http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=5&token=%s&cb=%s&id=%s&type=k&authorityType=&_=1535355856791' % (token, jQ, id)
+        cb = 'jQuery183018506892540538078_'+str(thistime-2000)
+        newid = id+'2'
+        ktype = 'k'
+        authorityType=''
+        xiahuaxian = str(thistime-1000)
+        urls = r'http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=%d&token=%s&cb=%s&id=%s&type=%s&authorityType=%s&_=%s' % (rtntype,token, cb, newid,ktype,authorityType,xiahuaxian)
+    elif id[0:3]=='600' or id[0:3]=='601'or id[0:3]=='603': #上证
+        rtntype=5
+        token='4f1862fc3b5e77c150a2b985b12db0fd'
+        cb='jQuery183029016096929456525_'+str(thistime-2000)
+        newid=id+'1'
+        ktype='k'# 日k线
+        xiahuaxian=str(thistime-1000)
+        authorityType=''
+        urls = r'http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=%d&token=%s&cb=%s&id=%s&type=%s&authorityType=%s&_=%s' % (rtntype,token, cb, newid,ktype,authorityType,xiahuaxian)
 
-
-
+#     print(urls)
     data=req.get(url=urls)
     data_txt=data.content
+#     print(data_txt)
     data_txt=data_txt.decode()
-    loc=data_txt.find(jQ)
+    loc=data_txt.find('({')
+#     print(loc)
     data_json=json.loads(data_txt[loc+1:-1])
     data_list=[]
     for i in range(len(data_json['data'])):
         data_list.append(data_json['data'][i].split(","))
         data_list[i].extend([id,data_json['name']])
     data_pd = pd.core.frame.DataFrame(data_list)
-    # data_pd.rename(columns={},)
+    data_pd.rename(columns={0:'date',1:'open',2:'close',3:'high',4:'low',5:'volume',6:'volume_rmb',7:'am',8:'turnoverrate',9:'code',10:'name'},inplace = True)
     return data_pd
 
 
 
 
 if __name__=='__main__':
-    data=cwzy_get_hist_data('0001121')
+    data=cwzy_get_hist_data('600821')
     time.sleep(1)
